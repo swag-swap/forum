95a30e32b0784db43b952cf1ad0694c502ba4e69
Swapnil Deshmukh <deshmukhswapnil316@gmail.com>
2024-02-22 23:56:42 +0530

    Changing storage type

--- forum/settings.py
+++ forum/settings.py
@@ -1,22 +1,37 @@
 import os
-from dotenv import load_dotenv
- 
-load_dotenv() 
-
 from pathlib import Path
+import environ
+# Initialise environment variables
+env = environ.Env(
+    DEBUG=(bool, True)
+)
+ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = env('SECRET_KEY')
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+ 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', 'True')=="True"
-DEBUG =True
+DEBUG = env('TEMP')
+print(BASE_DIR,DEBUG)
 ALLOWED_HOSTS = ['localhost:8000','forum-qomi.onrender.com','127.0.0.1']
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = env('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME) 
 
+
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api	
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY': env('API_KEY'),
+    'API_SECRET': env('API_SECRET')
+} 
+ 
 
 # Application definition
 
@@ -26,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage', 
     'django.contrib.staticfiles',
+    'cloudinary', 
     'app.apps.AppConfig',
     'ckeditor', 
     'crispy_forms', 
@@ -62,15 +79,14 @@ TEMPLATES = [
 ]
 
 
-WSGI_APPLICATION = 'forum.wsgi.application'
-
+WSGI_APPLICATION = 'forum.wsgi.application' 
 
 # Database 
 import dj_database_url
 # if not DEBUG:
-#     DATABASES = {
-#         'default': dj_database_url.config(default='postgres://forumdb_ky27_user:epAZ39NpyHuXB83BJgOM1ey0ZPbotQn2@dpg-cn710cuct0pc73fhgdmg-a.oregon-postgres.render.com/forumdb_ky27') 
-#     }
+DATABASES = {
+    'default': dj_database_url.config(default=env.db()) 
+}
 # else:
 #     print("Postgres URL not found, using sqlite instead")
 #     DATABASES = {
@@ -80,12 +96,6 @@ import dj_database_url
 #         }
 #     }
  
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 AUTH_USER_MODEL = 'app.CustomUser'
 
@@ -93,7 +103,7 @@ AUTH_USER_MODEL = 'app.CustomUser'
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [ 
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -122,14 +132,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-import os 
+# https://docs.djangoproject.com/en/5.0/howto/static-files/ 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'assets'
+STATIC_ROOT = BASE_DIR
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media_forum/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -143,8 +151,11 @@ CKEDITOR_CONFIGS = {
 }
 
 
+print(env('CLOUD_NAME'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
